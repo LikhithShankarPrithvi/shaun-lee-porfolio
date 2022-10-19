bd9e1dc8d520957ae7c444eec5551b58e923d673
Likhith Shankar <46518029+LikhithShankarPrithvi@users.noreply.github.com>
2022-10-20 02:46:03 +0530

    Added Header

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+const Header = () => {
+	return (
+		<>
+			<div className='flex justify-evenly m-3 p-4 pb-5 shadow shadow-white text-xl hover:shadow-slate-600'>
+				<div className='flex'>
+					<div className='cursor-pointer text-white font-bold md:pr-10 '>
+						Shaun Lee
+					</div>
+					<div className='hidden md:flex text-white font-bold'>|</div>
+				</div>
+				<div className='hidden md:flex flex-row justify-between text-white md:[&>*]:px-3 [&>*]:px-4 [&>*]:cursor-pointer '>
+					<div className='hover:text-black hover:bg-white hover:shadow-sm hover:shadow-slate-600 rounded-md pb-1'>
+						Home
+					</div>
+					<div className='hover:text-black hover:bg-white hover:shadow-sm hover:shadow-slate-600 rounded-md pb-1'>
+						About
+					</div>
+					<div className='hover:text-black hover:bg-white hover:shadow-sm hover:shadow-slate-600 rounded-md pb-1'>
+						Projects
+					</div>
+					<div className='hover:text-black hover:bg-white hover:shadow-sm hover:shadow-slate-600 rounded-md pb-1'>
+						Experience
+					</div>
+					<div className='hover:text-black hover:bg-white hover:shadow-sm hover:shadow-slate-600 rounded-md pb-1'>
+						Contact
+					</div>
+				</div>
+			</div>
+		</>
+	)
+}
+
+export default Header
